bddf376b0f6dd5d557ae0aaa5939e9084badf45d
Juan Diego <jdsoteldo00@gmail.com>
2019-08-30 11:51:39 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'items#index'
+  resources :items
+
+get 'about', to: "home#about"
+get 'contact', to: "home#contact"
+get 'hoodies', to: "items#hoodies"
+get 'shirts', to: "items#shirts"
+get 'accessories', to: "items#accessories"
+get 'long_sleeves', to: "items#long_sleeves"
+
 end
